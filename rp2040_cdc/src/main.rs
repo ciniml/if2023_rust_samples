9fc9464a69af7f63df4bddee29af225f3dc63f8f
Kenta IDA <fuga@fugafuga.org>
2023-01-02 18:01:09 +0900

    fmt

--- rp2040_cdc/src/main.rs
+++ rp2040_cdc/src/main.rs
@@ -17,30 +17,20 @@
 #![no_std]
 #![no_main]
 
-use core::cell::RefCell;
-
 use hal::pac;
 use panic_halt as _;
 use rp_pico::hal;
 
-use hal::usb::UsbBus;
 use usb_device::bus::UsbBusAllocator;
 use usb_device::prelude::*;
 use usbd_serial::{SerialPort, USB_CLASS_CDC};
-use hal::pac::interrupt;
-
-use cortex_m::interrupt::Mutex;
-
-static mut USB_BUS_ALLOCATOR: Option<UsbBusAllocator<hal::usb::UsbBus>> = None;
-static USB_DEVICE: Mutex<RefCell<Option<UsbDevice<'static, UsbBus>>>>= Mutex::new(RefCell::new(None));
-static USB_SERIAL: Mutex<RefCell<Option<SerialPort<'static, UsbBus>>>> = Mutex::new(RefCell::new(None));
 
 #[rp_pico::hal::entry]
 fn main() -> ! {
     let pac = pac::Peripherals::take().unwrap();
 
     let mut resets = pac.RESETS;
-    
+
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
     let clocks = hal::clocks::init_clocks_and_plls(
         rp_pico::XOSC_CRYSTAL_FREQ,
@@ -61,11 +51,9 @@ fn main() -> ! {
         true,
         &mut resets,
     ));
-    unsafe { USB_BUS_ALLOCATOR = Some(usb_allocator); }
 
-    let usb_allocator = unsafe { USB_BUS_ALLOCATOR.as_ref().unwrap() };
-    let usb_serial = SerialPort::new(usb_allocator);
-    let usb_device = UsbDeviceBuilder::new(usb_allocator, UsbVidPid(0x6666, 0x4444))
+    let mut usb_serial = SerialPort::new(&usb_allocator);
+    let mut usb_device = UsbDeviceBuilder::new(&usb_allocator, UsbVidPid(0x6666, 0x4444))
         .manufacturer("test manufacturer")
         .product("test product")
         .serial_number("serial number")
@@ -73,54 +61,27 @@ fn main() -> ! {
         .composite_with_iads()
         .max_packet_size_0(64)
         .build();
-    
-    cortex_m::interrupt::free(|cs| {
-        *USB_DEVICE.borrow(cs).borrow_mut() = Some(usb_device);
-        *USB_SERIAL.borrow(cs).borrow_mut() = Some(usb_serial);
-    });
-
-    // unsafe {
-    //     pac::NVIC::unmask(pac::Interrupt::USBCTRL_IRQ);
-    //     cortex_m::interrupt::enable();
-    // }
 
     let mut buffer = [0u8; 64];
     let mut pending_bytes_to_write = None;
     loop {
         // Loopback serial data
-        cortex_m::interrupt::free(|cs| {
-            let usb_serial = USB_SERIAL.borrow(cs);
-            if pending_bytes_to_write.is_none() {
-                if let Ok(bytes_read) = usb_serial.borrow_mut().as_mut().unwrap().read(&mut buffer) {
-                    pending_bytes_to_write = Some((0, bytes_read))
+        if pending_bytes_to_write.is_none() {
+            if let Ok(bytes_read) = usb_serial.read(&mut buffer) {
+                pending_bytes_to_write = Some((0, bytes_read))
+            }
+        }
+        if let Some((bytes_written, bytes_to_write)) = pending_bytes_to_write {
+            if let Ok(bytes_written_now) = usb_serial.write(&buffer[bytes_written..bytes_to_write])
+            {
+                let bytes_written = bytes_written + bytes_written_now;
+                if bytes_written == bytes_to_write {
+                    pending_bytes_to_write = None;
+                } else {
+                    pending_bytes_to_write = Some((bytes_written, bytes_to_write));
                 }
             }
-            if let Some((bytes_written, bytes_to_write)) = pending_bytes_to_write {
-                if let Ok(bytes_written_now) = usb_serial.borrow_mut().as_mut().unwrap().write(&buffer[bytes_written..bytes_to_write]) {
-                    let bytes_written = bytes_written + bytes_written_now;
-                    if bytes_written == bytes_to_write {
-                        pending_bytes_to_write = None;
-                    } else {
-                        pending_bytes_to_write = Some((bytes_written, bytes_to_write));
-                    }
-                }
-            }
-        });
-        //cortex_m::asm::wfi();
-        let _poll_result = cortex_m::interrupt::free(|cs|{
-            let usb_serial = USB_SERIAL.borrow(cs);
-            let usb_device = USB_DEVICE.borrow(cs);
-            usb_device.borrow_mut().as_mut().unwrap().poll(&mut [usb_serial.borrow_mut().as_mut().unwrap()])
-        });
+        }
+        usb_device.poll(&mut [&mut usb_serial]);
     }
-
-}
-
-#[interrupt]
-fn USBCTRL_IRQ() { 
-    let _poll_result = cortex_m::interrupt::free(|cs|{
-        let usb_serial = USB_SERIAL.borrow(cs);
-        let usb_device = USB_DEVICE.borrow(cs);
-        usb_device.borrow_mut().as_mut().unwrap().poll(&mut [usb_serial.borrow_mut().as_mut().unwrap()])
-    });
 }
