fe1c6d32c02a7b96c12c805b01680d7dfe765ee3
Kenta IDA <fuga@fugafuga.org>
2023-01-03 04:07:01 +0900

    Add comments

--- rp2040_cdc/src/main.rs
+++ rp2040_cdc/src/main.rs
@@ -23,7 +23,7 @@ use rp_pico::hal;
 
 use usb_device::bus::UsbBusAllocator;
 use usb_device::prelude::*;
-use usbd_serial::{SerialPort, USB_CLASS_CDC};
+use usbd_serial::SerialPort;
 
 #[rp_pico::hal::entry]
 fn main() -> ! {
@@ -32,6 +32,7 @@ fn main() -> ! {
     let mut resets = pac.RESETS;
 
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+    // クロックを初期化
     let clocks = hal::clocks::init_clocks_and_plls(
         rp_pico::XOSC_CRYSTAL_FREQ,
         pac.XOSC,
@@ -43,45 +44,54 @@ fn main() -> ! {
     )
     .ok()
     .unwrap();
+    // UsbBusを初期化
+    let usb_bus = hal::usb::UsbBus::new(
+        pac.USBCTRL_REGS,   // RP2040のUSBペリフェラルのレジスタ
+        pac.USBCTRL_DPRAM,  // RP2040のUSBペリフェラルのDPRAM
+        clocks.usb_clock,   // USBクロック
+        true,               // Vbus検出ビットを強制的にセットする
+        &mut resets,        // サブシステムのリセット・レジスタ
+    );
+    // UsbBusAllocatorを構築
+    // ※UsbBusAllocatorは内部可変性を持つ型なのでmutでなくて良い
+    let usb_bus_allocator = UsbBusAllocator::new(usb_bus);
+    // usb-serialクレートのSerialPortを構築
+    let mut usb_serial = SerialPort::new(&usb_bus_allocator);
+    // UsbDeviceを構築 VID=0x6666, PID=0x4444 (prototype product)
+    let mut usb_device = UsbDeviceBuilder::new(&usb_bus_allocator, UsbVidPid(0x6666, 0x4444))
+        .manufacturer("test manufacturer")  // Manufacturer  = "test manufacturer"
+        .product("test product")            // Product       = "test product"
+        .serial_number("serial number")     // Serial Number = "serial number" 
+        .composite_with_iads()              // IADを使った複合デバイスとする
+        .max_packet_size_0(64)              // 最大パケットサイズ (64バイト)
+        .build();                           // 上記の設定でUsbDeviceを構築
 
-    let usb_allocator = UsbBusAllocator::new(hal::usb::UsbBus::new(
-        pac.USBCTRL_REGS,
-        pac.USBCTRL_DPRAM,
-        clocks.usb_clock,
-        true,
-        &mut resets,
-    ));
-
-    let mut usb_serial = SerialPort::new(&usb_allocator);
-    let mut usb_device = UsbDeviceBuilder::new(&usb_allocator, UsbVidPid(0x6666, 0x4444))
-        .manufacturer("test manufacturer")
-        .product("test product")
-        .serial_number("serial number")
-        .device_class(USB_CLASS_CDC)
-        .composite_with_iads()
-        .max_packet_size_0(64)
-        .build();
-
+    // ループバック用のバッファ
     let mut buffer = [0u8; 64];
-    let mut pending_bytes_to_write = None;
+    // ループバック用バッファ内の送り返し待ちデータ
+    // ※本来は型指定不要で後続の文から型が推論される
+    let mut pending_bytes_to_write: Option<(usize, usize)> = None;
     loop {
-        // Loopback serial data
-        if pending_bytes_to_write.is_none() {
+        // USBシリアルのホストからの受信データを読み出して送り返す
+        if pending_bytes_to_write.is_none() {   // 送り返し待ちデータなければ読む
             if let Ok(bytes_read) = usb_serial.read(&mut buffer) {
-                pending_bytes_to_write = Some((0, bytes_read))
+                pending_bytes_to_write = Some((0, bytes_read))  // 送り返し待ちデータ残量保存
             }
         }
-        if let Some((bytes_written, bytes_to_write)) = pending_bytes_to_write {
+        if let Some((bytes_written, bytes_to_write)) = pending_bytes_to_write { // 送り返し待ちデータあり？
+            // バッファ内のデータを送信
             if let Ok(bytes_written_now) = usb_serial.write(&buffer[bytes_written..bytes_to_write])
             {
+                // 送り返し待ちデータの送信完了位置を更新
                 let bytes_written = bytes_written + bytes_written_now;
-                if bytes_written == bytes_to_write {
+                if bytes_written == bytes_to_write {    // 全部送り終わったらNoneにしておく
                     pending_bytes_to_write = None;
-                } else {
+                } else {                                // 残ってるので更新
                     pending_bytes_to_write = Some((bytes_written, bytes_to_write));
                 }
             }
         }
+        // USBデバイスのイベントなどを処理する
         usb_device.poll(&mut [&mut usb_serial]);
     }
 }
